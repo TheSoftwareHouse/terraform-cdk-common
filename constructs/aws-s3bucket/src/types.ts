763603347edfed9dc5d6d3ee6c9c2c7167074884
wwojcik <wojciech.wojcik@coolsoft.cloud>
2022-11-08 13:05:57 +0100

    change directory name

--- constructs/aws-s3bucket/src/types.ts
+++ constructs/aws-s3bucket/src/types.ts
@@ -1,4 +1,4 @@
-import { AwsResourceProps } from "aws-common-tools/lib/types";
+import { AwsResourceProps } from "../../../libs/aws-common-tools/lib/types";
 export interface AwsS3BucketProps extends AwsResourceProps {
     versioningEnabled: boolean;
 }
